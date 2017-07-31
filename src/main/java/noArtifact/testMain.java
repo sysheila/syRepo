7f299e7c2cc2bc1135efbabd23c5859e20a51b4a
SYSH <SYSH@ZHA-ITA008-W7.corp.oocl.com>
2017-07-31 16:42:19 +0800

    test dependency

---
+++ src/main/java/noArtifact/testMain.java
@@ -0,0 +1,12 @@
+
+
+package noArtifact;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Hello test!" );
+	}
+
+}
---
+++ src/test/java/noArtifact/testTheTestCase.java
@@ -0,0 +1,14 @@
+package noArtifact;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testTheTestCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
